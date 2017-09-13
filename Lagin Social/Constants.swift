f029839c97bc3a8ea4a50a5ee03dd30929b0e0fc
Lagin Riecker <30928319+LaginRiecker7@users.noreply.github.com>
2017-09-13 09:47:17 +0100

    Sign in ui custom classes complete

---
+++ Lagin Social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Lagin Social
+//
+//  Created by Lagin Riecker on 13/09/2017.
+//  Copyright © 2017 Football Crazy LTD. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ Lagin Social/CustomView.swift
@@ -0,0 +1,22 @@
+//
+//  CustomView.swift
+//  Lagin Social
+//
+//  Created by Lagin Riecker on 13/09/2017.
+//  Copyright © 2017 Football Crazy LTD. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(displayP3Red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        
+    }
+}
